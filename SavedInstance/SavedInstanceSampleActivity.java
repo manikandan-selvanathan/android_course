5f5ee5d4bd3de26abd3c20a47528c3884d935e01
Manikandan Selvanathan <manikandan.selvanathan@lennoxintl.com>
2019-01-12 09:22:23 +0530

    new Changes

---
+++ SavedInstance/SavedInstanceSampleActivity.java
@@ -0,0 +1,47 @@
+package com.facebook.www.SavedInstance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.www.R;
+
+public class SavedInstanceSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_instance_sample_activty);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        String value=savedInstanceState.getString("sample_key",null);
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("sample_key","Values");
+        super.onSaveInstanceState(outState);
+    }
+}
