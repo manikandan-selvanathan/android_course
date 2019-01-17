f2484043894aedf46e46c6b56d11f5a7e2bff885
Manikandan Selvanathan <manikandan.selvanathan@lennoxintl.com>
2019-01-18 00:12:54 +0530

    new Changes

---
+++ ViewPager/TitleStripViewPager/ViewPagerTitleStripActivity.java
@@ -0,0 +1,28 @@
+package com.facebook.www.ViewPager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.www.R;
+
+import java.util.ArrayList;
+
+public class ViewPagerTitleStripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager_title_strip);
+        final ViewPager viewPager=findViewById(R.id.viewpager);
+        final ActionBar actionBar = getSupportActionBar();
+        ArrayList<Fragment> list=new ArrayList<>();
+        list.add(new Fragment1());
+        list.add(new Fragment2());
+        list.add(new Fragment1());
+
+        viewPager.setAdapter(new SimpleFragmentPagerAdapter(getSupportFragmentManager(),list));
+    }
+}
